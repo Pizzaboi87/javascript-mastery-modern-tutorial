94fa2220d64cba0b5ec9080dc2063bcc8772b1fa
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-02-16 13:58:33 +0200

    CardDeal component finished.

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,5 +1,27 @@
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
+
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" /> in few easy
+          steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5 text-justify`}>
+          Finding the best credit card is part art, part science. No single
+          credit card is better than all others in all categories — or for all
+          people. But we can help you find the perfect choice for you.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
